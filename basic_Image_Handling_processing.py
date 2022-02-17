ac71d90c9264fdc7f8ea6d62dac49b6627d263e1
Bruno Ribeiro <brunorchaves1@gmail.com>
2022-02-16 22:53:55 -0300

    some examples of image filters with scipy

--- basic_Image_Handling_processing.py
+++ basic_Image_Handling_processing.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from pylab import *
-import numpy
+import numpy as np
+from scipy.ndimage import filters
+from matplotlib import pyplot as plt
+import imtools
 
 # pil_im = Image.open('empire.jpg')
 
@@ -8,19 +11,17 @@ import numpy
 # region = pil_im.crop(box)
 # region = region.transpose(Image.ROTATE_180)
 # pil_im.paste(region,box)
-im = array(Image.open('empire.jpg'))
-print( im.shape, im.dtype)
-im = array(Image.open('empire.jpg').convert('L'),'f')
-print( im.shape, im.dtype)
+# im = array(Image.open('empire.jpg'))
+# print( im.shape, im.dtype)
 
-im2 = 255 - im#invert image
-im3 = (100/255)*im + 100 # clamp to interval 100...200
-im4 = 255.0*(im/255)**2
+# im2 = 255 - im#invert image
+# im3 = (100/255)*im + 100 # clamp to interval 100...200
+# im4 = 255.0*(im/255)**2
 
 #create figure
-figure()
-gray()
-contour(im,origin= 'image')
+# figure()
+# gray()
+# contour(im,origin= 'image')
 
 # x = [100,100,400,400]
 # y = [200,500,200,500]
@@ -28,9 +29,34 @@ contour(im,origin= 'image')
 
 # plot(x[:2],y[:2])
 # title('Plotting: "empire.jpg"')
-axis('equal')
-axis('off')
+# axis('equal')
+# axis('off')
 
-figure()
-hist(im.flatten(),128)
-show()
+# figure()
+# hist(im.flatten(),128)
+
+# im_resized= imtools.imresize(im, uint16)
+
+im = array(Image.open('empire.jpg'))
+
+imx = np.zeros(im.shape,dtype=np.float64)
+filters.sobel(im,1,imx)
+imy = np.zeros(im.shape,dtype=np.float64)
+filters.sobel(im,0,imy)
+# imy = zeros(im.shape)
+# filters.gaussian_filter(im,(sigma,sigma),(1,0),imy)
+
+magnitude=np.sqrt(imx**2+imy**2)
+plt.subplot(2,2,1)
+plt.imshow(im,cmap=plt.cm.gray)
+plt.title("Original")
+plt.subplot(2,2,2)
+plt.imshow(magnitude,cmap=plt.cm.gray)
+plt.title("Magnitude of Gradient")
+plt.subplot(2,2,3)
+plt.imshow(imx,cmap=plt.cm.gray)
+plt.title('Derivative in x-direction')
+plt.subplot(2,2,4)
+plt.title('Derivative in y-direction')
+plt.imshow(imy,cmap=plt.cm.gray)
+plt.show()
--- imtools.py
+++ imtools.py
@@ -1,6 +1,44 @@
-# import os
+import os
+from PIL import Image
+from pylab import *
+import numpy
 
-# def get_imlist(path):
-#     """" Retruns a list of filenames for
-#     all jpg images in a dorectory"""
-#     return[os.path.join(path,f) for f in os listdir(path) if f.endswith('.jpg')]
+def get_imlist(path):
+    """" Retruns a list of filenames for
+    all jpg images in a dorectory"""
+    return[os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+from numpy import array, uint8
+
+# resize any image
+def imresize(im,sz):
+    pil__im = Image.fromarray(uint8(im))
+    return array(pil__im.resize(sz))
+
+
+## gives more details , more contrast, good for usage before further processing
+def histeq(im, nbr_bins = 256):
+    #"""" Histogram equalization of a grayscale image"""
+    #get image histogram
+    imhist, bins = histogram(im.flatten(),nbr_bins,density = True)
+    cdf = imhist.cumsum() #cumukatuve distribution function
+    cdf = 255*cdf / cdf[-1]#normalize
+    #use linear interpolation of cdf to find new pixel values
+    im2 = interp(im.flatten(),bins[:-1],cdf)
+
+    return im2.reshape(im.shape),cdf
+
+def compute_average(imlist):
+    """Compute the average of a list of images"""
+    #open first image and make into array of type float
+    averageim = array(Image.open[0],'f')
+
+    for imname in imlist[1:]:
+        try :
+            averageim += array(Image.open(imname))
+        except:
+            print(imname + '...skiped')
+    averageim /= len(imlist)
+
+    #return average as uint8
+    return array(averageim,'uint8')
